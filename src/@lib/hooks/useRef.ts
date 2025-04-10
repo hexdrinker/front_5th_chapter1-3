2a70c8e1cc50059c63b53d93b6f361f74828fab1
Park YuongHo <hexdrinker2020@gmail.com>
2025-04-11 01:06:29 +0900

    fix: lazy initialization 적용

--- src/@lib/hooks/useRef.ts
+++ src/@lib/hooks/useRef.ts
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
 export function useRef<T>(initialValue: T): { current: T } {
-  const [ref] = useState<{ current: T }>({ current: initialValue });
+  const [ref] = useState<{ current: T }>(() => ({ current: initialValue }));
   return ref;
 }
